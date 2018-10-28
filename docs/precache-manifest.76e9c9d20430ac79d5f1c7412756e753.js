85b91bfb4d2e578da8073d93e3fe42931dfb0e08
Daniel <daniel.d.ilievski@gmail.com>
2018-10-28 20:58:59 +0100

    added /doc

---
+++ docs/precache-manifest.76e9c9d20430ac79d5f1c7412756e753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "e26ea2847124bf9cd039",
+    "url": "/portfolio/static/js/main.e26ea284.chunk.js"
+  },
+  {
+    "revision": "a25b08967e493f728094",
+    "url": "/portfolio/static/js/1.a25b0896.chunk.js"
+  },
+  {
+    "revision": "e26ea2847124bf9cd039",
+    "url": "/portfolio/static/css/main.013f30e3.chunk.css"
+  },
+  {
+    "revision": "8e381cd1e26c64cca2f84073cea6f22f",
+    "url": "/portfolio/index.html"
+  }
+];
