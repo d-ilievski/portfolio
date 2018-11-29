226f1bd126c547080f08b96bacd8c02ba442184d
Daniel <daniel.d.ilievski@gmail.com>
2018-11-29 03:41:59 +0100

    Updates

---
+++ precache-manifest.7483e95c37280c394186bc5388add36a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b80bab4cecfecbc08ae6cfbc7b4089c7",
+    "url": "/portfolio/static/media/daniel.b80bab4c.JPG"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "d677ef699bd4db6e3368",
+    "url": "/portfolio/static/js/main.d677ef69.chunk.js"
+  },
+  {
+    "revision": "a9fb69306482ea1971a4",
+    "url": "/portfolio/static/js/1.a9fb6930.chunk.js"
+  },
+  {
+    "revision": "d677ef699bd4db6e3368",
+    "url": "/portfolio/static/css/main.efb197bc.chunk.css"
+  },
+  {
+    "revision": "f49a3f140cb39590bd12fa5fc9230efe",
+    "url": "/portfolio/index.html"
+  }
+];
